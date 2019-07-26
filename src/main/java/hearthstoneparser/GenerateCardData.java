6cb292e1b79bd253772b1e3e14654ef85fbae9f3
sebastientromp <sebastien.tromp@gmail.com>
2019-07-26 15:27:09 +0200

    udpate for 14.6.0.31761

--- src/main/java/hearthstoneparser/GenerateCardData.java
+++ src/main/java/hearthstoneparser/GenerateCardData.java
@@ -1,13 +1,15 @@
 package hearthstoneparser;
 
-import com.github.slugify.Slugify;
 import com.google.common.collect.Lists;
-import org.apache.commons.lang.StringUtils;
 import org.apache.commons.lang.WordUtils;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.json.simple.parser.JSONParser;
 
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.InputStream;
@@ -25,8 +27,8 @@ import java.util.stream.Collectors;
 
 public class GenerateCardData {
 
-	private static final boolean FETCH_IMAGES = false;
-	private static final String PYTHON_UNITYPACK_AUDIO_OUT_DIRE = "G:\\Source\\hearthsim\\python-unitypack\\out\\audio";
+	private static final boolean FETCH_IMAGES = true;
+	private static final String PYTHON_UNITYPACK_AUDIO_OUT_DIRE = "G:\\Source\\hearthsim\\python-unitypack\\out\\audio2";
 	private static final Map<String, String> SET_CODES = buildSetCodes();
 
 	private static Map<String,String> buildSetCodes() {
@@ -60,6 +62,7 @@ public class GenerateCardData {
 				.map(GenerateCardData::flattenDirectoryStructure).flatMap(List::stream)
 				.map(File::getName)
 				.collect(Collectors.toSet());
+//        Set<String> existingImages = new HashSet<>();
 
 		System.out.println("init done " + referenceCards.length() + ", " + soundEffects.length() + ", " + audioClips.size());
 		JSONObject card = null;
@@ -91,6 +94,11 @@ public class GenerateCardData {
 					card.put("text", originalText.getString("enUS"));
 				}
 
+				JSONObject originalCollectionText = (JSONObject) card.remove("collectionText");
+				if (originalCollectionText != null && originalCollectionText.has("enUS")) {
+					card.put("collectionText", originalCollectionText.getString("enUS"));
+				}
+
 				if (card.has("flavor")) {
 					card.put("flavor", card.getJSONObject("flavor").getString("enUS"));
 				}
@@ -119,9 +127,9 @@ public class GenerateCardData {
 				if (!FETCH_IMAGES) {
 					continue;
 				}
-				if (!id.startsWith("DAL")) {
-					continue;
-				}
+//				if (!id.startsWith("DAL")) {
+//					continue;
+//				}
 
 				String imageName = id + ".png";
 				try {
@@ -133,8 +141,8 @@ public class GenerateCardData {
 					System.out.println("File doesn't exist, moving on: " + imageName);
 					// Download the card
 					InputStream in = getInputStream(card);
-					Files.copy(in, Paths.get("images/en/" + imageName));
-					long imageSize = Files.size(Paths.get("images/en/" + imageName));
+					Files.copy(in, Paths.get("images/" + imageName));
+					long imageSize = Files.size(Paths.get("images/" + imageName));
 					if (imageSize > 0) {
 						// Update the card
 						card.put("cardImage", imageName);
@@ -142,72 +150,19 @@ public class GenerateCardData {
 						System.out.println("Downloaded card for " + id);
 					}
 					else {
-						Paths.get("images/en/" + imageName).toFile().delete();
+						Paths.get("images/" + imageName).toFile().delete();
 						System.out.println("Empty image: " +imageName);
 					}
 				}
 				catch (FileAlreadyExistsException e) {
-//					 System.out.println("\tImage already exists for: " + id);
 					 card.put("cardImage", imageName);
 				}
 				catch (Exception e) {
-//					System.out.println("Image does not exist " + imageName + " at path " + e.getMessage());
 					if (imageName.startsWith("DAL")) {
 						System.err.println("Could not find Dalaran image! " + e.getMessage());
 					}
-					// e.printStackTrace();
 				}
-
-//				imageName = id + ".gif";
-//				spec = "http://media.services.zam.com/v1/media/byName/hs/cards/enus/animated/" + id
-//							+ "_premium.gif" + "?12576";
-//				try {
-//					if (Paths.get("images/golden/" + id + ".gif").toFile().exists()) {
-//						throw new FileAlreadyExistsException(Paths.get("images/golden/" + id + ".gif").toString());
-//					}
-//					if (Paths.get("images/golden/old/" + id + ".gif").toFile().exists()) {
-//						throw new FileAlreadyExistsException(Paths.get("images/golden/old/" + id + ".gif").toString());
-//					}
-//					if (Paths.get("images/golden/new/" + id + ".gif").toFile().exists()) {
-//						throw new FileAlreadyExistsException(Paths.get("images/golden/new" + id + ".gif").toString());
-//					}
-//					if (Paths.get("images/golden/new_UNG/" + id + ".gif").toFile().exists()) {
-//						throw new FileAlreadyExistsException(Paths.get("images/golden/new_UNG" + id + ".gif").toString());
-//					}
-//					// Get the golden image
-//					// Download the card
-//					URL url = new URL(spec);
-//					InputStream in = url.openStream();
-//					Files.copy(in, Paths.get("images/golden/" + id + ".gif"));
-//					long imageSize = Files.size(Paths.get("images/golden/" + id + ".gif"));
-//					if (imageSize > 0) {
-//						// Update the card
-//						card.put("goldenImage", id + ".gif");
-//						goldImageMap.put(id, id + ".gif");
-//						System.out.println("Golden image downloaded for " + id);
-//					}
-//					else {
-//						Paths.get("images/golden/" + id + ".gif").toFile().delete();
-//						System.out.println("\tEmpty image, skipping");
-//					}
-//					in.close();
-//				}
-//				catch (FileAlreadyExistsException e) {
-//					// Update the card
-//					 System.out.println("\tGolden image already exists for: " + id);
-//					card.put("goldenImage", id + ".gif");
-//					goldImageMap.put(id, id + ".gif");
-//				}
-//				catch (Exception e) {
-//					System.out.println("Golden mage does not exist " + imageName + " at path " + spec);
-//					// e.printStackTrace();
-//				}
 			}
-//			System.out.println(imageMap);
-//			System.out.println(goldImageMap);
-//			System.out.println(cardsArtMap);
-
-//			System.out.println(audioClips);
 			System.out.println(referenceCards);
 		}
 		catch (Exception e) {
@@ -221,45 +176,65 @@ public class GenerateCardData {
 		return file.isDirectory() ? Arrays.asList(file.listFiles()) : Lists.newArrayList(file);
 	}
 
-	/** Try various combinations to download the image */
 	private static InputStream getInputStream(JSONObject card) throws Exception {
-		List<String> possibleSpecs = Lists.newArrayList(
-				"2018/11/",
-				"2018/10/",
-				"2018/08/",
-				"2019/04/",
-				"2019/03/"
-				);
-		String baseCardName = new Slugify().slugify(card.getString("name")
-				.replaceAll("'", "")
-				.replaceAll("\\.", ""));
-		List<String> possibleCardNames = Lists.newArrayList(
-				baseCardName,
-				baseCardName + "-300x414",
-				baseCardName + "-card-art-300x429",
-				baseCardName + "-temp-card-art-300x429",
-				StringUtils.capitalize(baseCardName),
-				WordUtils.capitalize(baseCardName, new char[] {'-'}));
-		for (String cardName : possibleCardNames) {
-			for (String possibleSpec : possibleSpecs) {
-				String spec = "https://www.hearthstonetopdecks.com/wp-content/uploads/"
-						+ possibleSpec
-						+ cardName
-						+ ".png";
-				try {
-					return getInputStream(spec);
-				}
-				catch (Exception e) {
-					// Image is not at correct location, trying something else
-				}
-			}
-		}
-		throw new Exception("Could not find image for card " + card.getString("id")
-				+ " with slugified name: " + possibleCardNames);
+        // Create a new trust manager that trust all certificates
+        TrustManager[] trustAllCerts = new TrustManager[]{
+                new X509TrustManager() {
+                    public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+                        return null;
+                    }
+                    public void checkClientTrusted(
+                            java.security.cert.X509Certificate[] certs, String authType) {
+                    }
+                    public void checkServerTrusted(
+                            java.security.cert.X509Certificate[] certs, String authType) {
+                    }
+                }
+        };
+
+        // Activate the new trust manager
+        try {
+            SSLContext sc = SSLContext.getInstance("SSL");
+            sc.init(null, trustAllCerts, new java.security.SecureRandom());
+            HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+        } catch (Exception e) {
+            System.err.println("Caught exception " + e.getMessage());
+        }
+
+        // And as before now you can use URL and URLConnection
+        System.setProperty("http.agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:66.0) Gecko/20100101 Firefox/66.0");
+        URL url = new URL("https://art.hearthstonejson.com/v1/render/latest/enUS/256x/" + card.getString("id") + ".png");
+        return url.openStream();
 	}
 
-	private static InputStream getInputStream(String spec) throws Exception {
-		URL url = new URL(spec);
-		return url.openStream();
-	}
+//    private static InputStream getInputStream(JSONObject card) throws Exception {
+//        String url = "https://art.hearthstonejson.com/v1/render/latest/enUS/256x/" + card.getString("id") + ".png";
+//
+//        SSLContextBuilder builder = new SSLContextBuilder();
+//        builder.loadTrustMaterial(null, new TrustSelfSignedStrategy());
+//        SSLConnectionSocketFactory sslsf = new SSLConnectionSocketFactory(builder.build(),
+//                SSLConnectionSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER) {
+//            @Override
+//            protected void prepareSocket(SSLSocket socket) throws IOException {
+//                try {
+//                    PropertyUtils.setProperty(socket, "host", "hsreplay.net");
+//                    socket.setEnabledProtocols(new String[] { "SSLv3", "TLSv1", "TLSv1.1", "TLSv1.2" });
+//                }
+//                catch (IllegalAccessException | NoSuchMethodException | InvocationTargetException ex) {
+//                    System.err.println("Exception " + ex.getMessage());
+//                }
+//                super.prepareSocket(socket);
+//            }
+//
+//        };
+//        CloseableHttpClient httpClient = HttpClients
+//                .custom()
+//                .setSSLSocketFactory(sslsf)
+//                .setDefaultRequestConfig(RequestConfig.custom().setCookieSpec(CookieSpecs.STANDARD).build())
+//                .build();
+//        HttpGet httpGet = new HttpGet(url);
+//        CloseableHttpResponse response1 = httpClient.execute(httpGet);
+//        HttpEntity entity1 = response1.getEntity();
+//        return entity1.getContent();
+//    }
 }
